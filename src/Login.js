f138c4808b52ee4cf071ecffa6a4128e43a96551
piggy2303 <nguyentuananh23031997@gmail.com>
2018-04-04 17:16:45 +0700

    gan xong login

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {  View, Text, StyleSheet,Image, Dimensions} from 'react-native';
+import FormLogin from './component/FormLogin';
+
 
 const {width,height} = Dimensions.get('screen');
 
@@ -8,7 +10,7 @@ export default class Login extends Component {
     return (
       <View style = {styles.View_background}>
         <Image style={styles.centerImage} source={require('../img/iconUser/iconUser.png')} />
-        
+        <FormLogin />
       </View>
     );
   }
@@ -18,10 +20,9 @@ const styles = StyleSheet.create({
   View_background:{
     backgroundColor: "rgb(133,201,197)",
     flex: 1,
-    // alignItems: 'center',
+    alignItems: 'center',
   },
   centerImage :{
-    alignSelf: 'center',
     marginTop: "15%",
   }
 })
--- src/component/FormLogin.js
+++ src/component/FormLogin.js
@@ -1,12 +1,85 @@
-import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
 
 export default class formLogin extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.Viewmain}>
+        <TextInput
+          style={styles.InputEmail}
+          placeholder="Email"
+          placeholderTextColor="white"
+          underlineColorAndroid="white"
+        />
+        <TextInput
+          style={styles.InputPassword}
+          placeholder="Password"
+          placeholderTextColor="white"
+          underlineColorAndroid="white"
+        />
+
+        <TouchableOpacity style={styles.TouchableOpacity_Login}>
+          <Text style={styles.Text_Login}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.TouchableOpacity_Signup}>
+          <Text style={styles.Text_Signup}>Sign-up</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  Viewmain: {
+    backgroundColor: "rgb(133,201,197)",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  InputEmail: {
+    width: 360,
+    borderColor: "rgb(133,201,197)",
+    borderWidth: 1,
+    shadowColor: "rgb(133,201,197)",
+    color: "white"
+    // fontStyle: "HelveticaNeue"
+  },
+  InputPassword: {
+    width: 360,
+    borderColor: "rgb(133,201,197)",
+    borderWidth: 1,
+    shadowColor: "rgb(133,201,197)",
+    marginTop: "3%",
+    color: "white"
+  },
+  TouchableOpacity_Login: {
+    backgroundColor: "white",
+    width: 360,
+    height: 54,
+    marginTop: "8%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 27,
+    //flex:2,
+  },
+  Text_Login:{
+    fontSize: 20,
+    color: "rgb(133,201,197)",
+    fontFamily: 'HelveticaNeue',
+  },
+  TouchableOpacity_Signup: {
+    marginTop: "8%"
+  },
+  Text_Signup:{
+    fontSize: 20,
+    color: "white",
+    fontFamily: 'HelveticaNeue',
+  },
+});
